56ad06bf77aa03f8494cc3f21ee7342ec66ac330
MD. ARIFUL ISLAM <arifjmamun24@gmail.com>
2017-01-31 15:02:26 +0600

    Project Initiated

---
+++ UCRMS/DAL/DBconnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace UCRMS.DAL
+{
+    public class DBconnection
+    {
+        protected SqlConnection Connection;
+        protected SqlCommand Command;
+        protected SqlDataReader Reader;
+        protected SqlDataAdapter Adapter;
+
+        public DBconnection()
+        {
+            Connection = new SqlConnection(WebConfigurationManager.ConnectionStrings[""].ConnectionString);
+            Command = new SqlCommand();
+            Command.Connection = Connection;
+        }
+    }
+}
