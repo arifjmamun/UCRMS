3b85b0e62e8c154c47df94a517919fe078c8ca58
MD. ARIFUL ISLAM <arifjmamun24@gmail.com>
2017-01-31 18:29:43 +0600

    Added Course entitymodel

---
+++ UCRMS/Models/EntityModels/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UCRMS.Models.EntityModels
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Credit { get; set; }
+        public string Description { get; set; }
+        public int DepartmentId { get; set; }
+        public int SemesterId { get; set; }
+        public byte Assigned { get; set; }
+    }
+}
