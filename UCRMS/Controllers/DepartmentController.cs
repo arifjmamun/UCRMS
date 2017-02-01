96f7e6ce46da628e2a103efb9fcdc567c3743124
MD. ARIFUL ISLAM <arifjmamun24@gmail.com>
2017-02-01 21:36:42 +0600

    Removed unnecessary files and modified several files and added Annotation in a model

---
+++ UCRMS/Controllers/DepartmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCRMS.Models.EntityModels;
+
+namespace UCRMS.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View();
+        }
+
+        // POST: Department
+        [HttpPost]
+        public ActionResult Save(Department department)
+        {
+            return View(department);
+        }
+
+        [HttpGet]
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+    }
+}
--- UCRMS/Models/EntityModels/Department.cs
+++ UCRMS/Models/EntityModels/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,13 @@ namespace UCRMS.Models.EntityModels
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department Code cannot be empty.")]
+        [StringLength(7,MinimumLength = 2, ErrorMessage = "Length - Max: 7 Char and Min: 2 Char.")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "Department Name cannot be empty.")]
+        [StringLength(60, ErrorMessage = "Maximum Length: 60 Char.")]
         public string Name { get; set; }
     }
 }
